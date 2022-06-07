9eaf1b3b7282f732e18d9bd2dba176e50da0ac4f
Buckeyes2019 <55885798+Buckeyes2019@users.noreply.github.com>
2022-06-06 20:20:13 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='topicx',
+      version='0.1',
+      description='Topic modeling library',
+      url='https://github.com/Buckeyes2019/topicx',
+      author='Topicx Paper Authors',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['topicx'],
+      install_requires=['octis', 'umap-learn', 'hdbscan', 'simcse==0.4', 'plotly', 'flair', 'gensim'],
+      zip_safe=False)
